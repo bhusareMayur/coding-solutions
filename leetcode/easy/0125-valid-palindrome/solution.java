2bc57079be4976924f119e1b4e6e7ec2fc06031c
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-08 15:07:10 +0530

    ✅ Valid Palindrome | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-08

---
+++ leetcode/easy/0125-valid-palindrome/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            char c = s.charAt(i);
+            char c2 = s.charAt(j);
+            if ((c < 'a' && c > 'z') || (c < 'A' && c > 'Z')) i++;
+            if ((c2 < 'a' && c2 > 'z') || (c2 < 'A' && c2 > 'Z')) j--;
+            if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
