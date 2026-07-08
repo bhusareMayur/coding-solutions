675b246361d87d1119c6ef10ba89441443e86422
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-08 12:41:36 +0530

    ✅ Median of Two Sorted Arrays | Hard | Java | Runtime: 0 ms | Memory: 45 MB | 2026-07-08

---
+++ leetcode/hard/0004-median-of-two-sorted-arrays/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // with space O(1)
+        int m = nums1.length;
+        int n = nums2.length;
+        int mn = m+n;
+
+        int prev = 0;
+        int curr = 0;
+        int i = 0 ; 
+        int j = 0;
+        int k = 0;
+        while(k != mn / 2){
+            prev = curr;
+            if(nums1[i] < nums2[j]){
+                curr = nums1[i];
+                i++;
+            }else{
+                curr = nums2[j];
+                j++;
+            }
+            k++;
+        }
+    if(mn % 2 == 0){
+        return (double)( prev + curr ) / 2.0;
+    }
+
+    return (double)curr;
+
+    }
+}
