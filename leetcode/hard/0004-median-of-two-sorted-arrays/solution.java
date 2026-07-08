caddf09408b4d8d23edb0fd458a2036932ec7d18
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-08 12:53:01 +0530

    ✅ Median of Two Sorted Arrays | Hard | Java | Runtime: 2 ms | Memory: 45.6 MB | 2026-07-08

--- leetcode/hard/0004-median-of-two-sorted-arrays/solution.java
+++ leetcode/hard/0004-median-of-two-sorted-arrays/solution.java
@@ -4,14 +4,13 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
         int mn = m+n;
-        
 
         int prev = 0;
         int curr = 0;
         int i = 0 ; 
         int j = 0;
         int k = 0;
-        while(k <= (mn / 2) && i < m && j < n){
+        while(k < (mn / 2)){
             prev = curr;
             if(nums1[i] < nums2[j]){
                 curr = nums1[i];
@@ -22,20 +21,8 @@ class Solution {
             }
             k++;
         }
-       while(k <= (mn / 2) && i < m ) {
-        prev = curr;
-         curr = nums1[i];
-        i++;
-        k++;
-       }
-       while(k <= (mn / 2) && j < n ) {
-        prev = curr;
-         curr = nums2[j];
-        j++;
-        k++;
-       }
-        // System.out.println("curr : "+curr);
-        // System.out.println("Prev : "+prev);
+        System.out.println("curr : "+curr);
+        System.out.println("Prev : "+prev);
     if(mn % 2 == 0){
         return (double)( prev + curr ) / 2.0;
     }
