4b56991dd22e90103e7d98d0add6bbc801591286
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-08 12:19:07 +0530

    ✅ 3Sum | Medium | Java | Runtime: 2 ms | Memory: 43 MB | 2026-07-08

--- leetcode/medium/0015-3sum/solution.java
+++ leetcode/medium/0015-3sum/solution.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         for(int i = 0 ; i <n-2;i++){
-            if(i > 0 && nums[i] == nums[i + 1]) continue;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             if(nums[i] > 0) break;
             int y = i + 1;
             int z = n - 1;
