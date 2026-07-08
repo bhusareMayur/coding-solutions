2489e9265d1f2b8cddb3680549fe3d4998c7d27a
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-08 12:18:20 +0530

    ✅ 3Sum | Medium | Java | Runtime: 1 ms | Memory: 42.9 MB | 2026-07-08

---
+++ leetcode/medium/0015-3sum/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0 ; i <n-2;i++){
+            if(i > 0 && nums[i] == nums[i + 1]) continue;
+            if(nums[i] > 0) break;
+            int y = i + 1;
+            int z = n - 1;
+            while(y<z){
+                int sum = nums[i] + nums[y] + nums[z];
+                if(sum == 0){
+                    ans.add((Arrays.asList(nums[i], nums[y], nums[z])));
+
+                    while(y < z && nums[y] == nums[y+1])y++;
+                    while(y<z && nums[z] == nums[z-1])z--;
+                    y++;
+                    z--;
+                }
+                else if(sum > 0){
+                    z--;
+                }else y++;
+            }
+        }
+        return ans;
+    }
+}
