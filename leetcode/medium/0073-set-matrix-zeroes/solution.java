34a42328b25cc2a1751bdc39e5f8f15622fb95c8
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-09 11:12:40 +0530

    ✅ Set Matrix Zeroes | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-07-09

--- leetcode/medium/0073-set-matrix-zeroes/solution.java
+++ leetcode/medium/0073-set-matrix-zeroes/solution.java
@@ -2,30 +2,23 @@ class Solution {
     public void setZeroes(int[][] m) {
         int m1 = m.length;
         int n = m[0].length;
-        HashMap<Integer,Integer> mp = new HashMap<>();
+        // HashMap<Integer,Integer> mp = new HashMap<>();
+        boolean row[] = new boolean[m1];
+        boolean col[] = new boolean[n];
+
 
         for(int i = 0 ; i <m1;i++){
             for(int j = 0 ; j <n;j++){
-                if(m[i][j] == 0) mp.put(i,j);
+                if(m[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
             }
         }
-
-        for(Map.Entry<Integer, Integer> e : mp.entrySet()){
-            int i = e.getKey();
-            int j = e.getValue();
-            
-            // row j++
-            for(int y = 0;y<n;y++){
-                m[i][y] = 0;
+        for(int i = 0 ; i <m1;i++){
+            for(int j = 0 ; j <n;j++){
+                if(row[i] == true || col[j] == true) m[i][j] = 0;
             }
-            // col i++
-            for(int y = 0;y<m1;y++){
-                m[y][j] = 0;
-            }
-
-        }
-
-
-        
+        }    
     }
 }
