44dfc9148eff2aa82fb7c41a614a16ea6d093123
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-09 10:28:23 +0530

    ✅ Jump Game | Medium | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-07-09

---
+++ leetcode/medium/0055-jump-game/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n){
+            if(j == n-1) return true;
+            j = j + nums[j];
+            i++;
+        }
+        return false;
+    }
+}
