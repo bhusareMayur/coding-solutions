ce72a1eb94f16870dc86bc2e5280329d08ee0e55
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-09 11:29:26 +0530

    ✅ Ones and Zeroes | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-09

---
+++ leetcode/medium/0474-ones-and-zeroes/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int count = 0;
+        for(int i = 0 ; i < strs.length;i++){
+            if(strs[i].length() <= Math.max(m,n)) count++;
+            else if(strs[i].length() > m + n)continue;
+            else{
+                int ones = 0;
+                int zeroes = 0;
+                for(int j = 0;j<strs[i].length();j++){
+                    if(strs[i].charAt(i) == '1')ones++;
+                    else zeroes++;
+                }
+                if(ones <= n && zeroes <= m) count++;
+            }
+        }
+        return count;
+    }
+}
