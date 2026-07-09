3a2cc16ea2516f219be0c54fa3db7f3f2c4301a9
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-07-09 12:10:55 +0530

    ✅ Search a 2D Matrix | Medium | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-07-09

--- leetcode/medium/0074-search-a-2d-matrix/solution.java
+++ leetcode/medium/0074-search-a-2d-matrix/solution.java
@@ -15,6 +15,14 @@ class Solution {
              int mid = i + (j - i) / 2;
             if(t >= arr[mid][0] && t <= arr[mid][n - 1]) {
                 row = mid;
+                i = 0;
+                j = n - 1;
+                while(i <= j){
+                   int mid2 =  i + (j - i) / 2;
+                     if(arr[row][mid2] == t) return true;
+                    else if(arr[row][mid2] < t) i = mid2+1;
+                    else j = mid2 -1;
+                 }
                 break;
             }
             if(arr[mid][0] > t){
