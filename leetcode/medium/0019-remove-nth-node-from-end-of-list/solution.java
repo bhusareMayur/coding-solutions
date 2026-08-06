a40109622b92f91e80fa6b1bad63df5831cee836
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-08-06 19:24:58 +0530

    ✅ Remove Nth Node From End of List | Medium | Java | Runtime: 3 ms (0.85%) | Memory: 43.6 MB | 2026-08-06

---
+++ leetcode/medium/0019-remove-nth-node-from-end-of-list/solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      ListNode temp = head;
+      int size = 1;
+      while(temp.next != null){
+        temp = temp.next;
+        size++;
+      }
+      
+      if(size == 1 && n == 1) return null;
+      if(size == n) return head.next;
+    System.out.println(size);
+    n = size - n;
+    temp = head;
+    for(int i = 0 ; i <n-1;i++){
+        temp = temp.next;
+    }
+    temp.next = temp.next.next;
+    System.out.println(temp.val);
+
+
+
+
+
+
+
+return head;
+
+    //   if(size == 1 && n == 1) return null;
+    //   System.out.println(size);
+    // if(size < n) return head;
+    //   n = size - n;
+    //   System.out.println(n);
+    //   temp = head;
+    //   for(int i = 1;i<n;i++){
+    //     temp = temp.next;
+    //   }
+    //   if(temp.next.next == null) temp.next = null;
+    //   else
+    //   temp.next = temp.next.next;
+
+    //     return head;
+    }
+}
