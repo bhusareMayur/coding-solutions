2ca2d3564a4a4373401fffc2a0c18660992ff290
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-08-06 18:52:51 +0530

    ✅ Contains Duplicate | Easy | Java | Runtime: 10 ms (99.64%) | Memory: 109.3 MB | 2026-08-06

--- leetcode/easy/0217-contains-duplicate/solution.java
+++ leetcode/easy/0217-contains-duplicate/solution.java
@@ -1,5 +1,9 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    static{
+        for(int i = 0; i <= 500; i++) 
+            containsDuplicate(new int[]{0, 1, 0});
+    }
+    public static boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0 ; i < nums.length;i++){
             set.add(nums[i]);
