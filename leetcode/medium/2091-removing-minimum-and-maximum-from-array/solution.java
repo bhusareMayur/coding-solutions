8de9b6bcd841ebab909e88c359148f3837b583e6
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-19 10:40:26 +0530

    ✅ Removing Minimum and Maximum From Array | Medium | Java | Runtime: 3 ms (71.98%) | Memory: 86.3 MB | 2026-09-19

--- leetcode/medium/2091-removing-minimum-and-maximum-from-array/solution.java
+++ leetcode/medium/2091-removing-minimum-and-maximum-from-array/solution.java
@@ -8,15 +8,11 @@ class Solution {
             if(nums[mini] > nums[i]) mini  = i;
             if(nums[maxi] < nums[i]) maxi = i; 
         }
-        // System.out.println("maxi :"+maxi);
-        // System.out.println("mini :"+mini);
 
         int w1 = maxi > mini ? maxi + 1 : mini + 1;
         int w2 = maxi < mini ? n - maxi : n- mini;
         int w3 = maxi < mini ? (maxi + 1 + (n - mini)) : (mini + 1 + (n - maxi));
-        //  System.out.println("w1 :"+w1);
-        //  System.out.println("w2 :"+w2);
-        //  System.out.println("w3 :"+w3);
+
         int ans = Integer.MAX_VALUE;
         if(ans > w1) ans = w1;
         if(ans > w2) ans = w2;
