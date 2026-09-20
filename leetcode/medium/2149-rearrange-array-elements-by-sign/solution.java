6625b52ad6a741f532a5fa8491f78a011152cc34
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-20 12:24:17 +0530

    ✅ Rearrange Array Elements by Sign | Medium | Java | Runtime: 3 ms (100.00%) | Memory: 175.7 MB | 2026-09-20

--- leetcode/medium/2149-rearrange-array-elements-by-sign/solution.java
+++ leetcode/medium/2149-rearrange-array-elements-by-sign/solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int[] ans = new int[nums.length];
-        int j =0;
-        for(int i =0;i<nums.length;i++){
-            if(nums[i] >= 0) {
+        int j = 0;
+        int k = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
                 ans[j] = nums[i];
                 j += 2;
-                }
-        }
-        j=1;
-        for(int i =0;i<nums.length;i++){
-            if(nums[i] < 0) {
-                ans[j] = nums[i];
-                j += 2;
-                }
+            } else {
+                ans[k] = nums[i];
+                k += 2;
+            }
         }
         return ans;
     }
