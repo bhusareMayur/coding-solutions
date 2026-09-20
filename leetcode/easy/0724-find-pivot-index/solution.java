c82b129a059a5e126ee5f458c43ab814489eb97b
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-20 11:39:21 +0530

    ✅ Find Pivot Index | Easy | Java | Runtime: 1 ms (96.27%) | Memory: 47.6 MB | 2026-09-20

---
+++ leetcode/easy/0724-find-pivot-index/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] ls = new int[n];
+        int[] rs = new int[n];
+        ls[0] = 0;
+
+        for(int i = 0 ; i < n-1;i++){
+            ls[i+1] = ls[i]+nums[i];
+        }
+        rs[n-1]=0;
+        for(int i = n-1 ; i >= 1;i--){
+            rs[i-1] = rs[i]+nums[i];
+        }
+        for(int i = 0 ; i < n;i++){
+            if(rs[i]==ls[i])return i;
+        }
+        return -1;
+    }
+}
