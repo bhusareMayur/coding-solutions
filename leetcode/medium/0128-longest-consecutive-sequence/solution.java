45dbdee02c6d316d04ce38feb3de679319d4a9b9
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-20 17:43:46 +0530

    ✅ Longest Consecutive Sequence | Medium | Java | Runtime: 21 ms (95.96%) | Memory: 77.6 MB | 2026-09-20

--- leetcode/medium/0128-longest-consecutive-sequence/solution.java
+++ leetcode/medium/0128-longest-consecutive-sequence/solution.java
@@ -1,24 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         Arrays.sort(nums);
+        if(nums.length == 0 )return 0;
         if(nums.length == 1) return 1;
-        int maxLength=0;
-        int l = 0;
-        int r = 0;
-        int d = 0;
+       
+        int maxLength=1;
+        int currLength = 1;
         for(int i = 1;i<nums.length;i++){
-            if(nums[i] == nums[i-1]) {
-                d++;
-                continue;
-            }
+            if(nums[i] == nums[i-1]) continue;
 
             if(nums[i] == nums[i-1]+1){
-                r=i;
+                currLength++;
             }else{
-                l=i;
+               maxLength = Math.max(currLength,maxLength);
+               currLength=1;
             }
-            maxLength = Math.max(maxLength,r-l+1-d);
         }
-        return maxLength;
+        return Math.max(currLength,maxLength);
     }
 }
