c375c172c5558d8070434eee6fe69b32dae67a18
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-21 14:57:56 +0530

    ✅ Rotate Image | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.7 MB | 2026-09-21

---
+++ leetcode/medium/0048-rotate-image/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] m) {
+        int n = m.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = temp;
+            }
+        }
+
+        for(int i = 0 ; i < n;i++){
+            int j = 0;
+            int k = n-1;
+            while(j < k){
+                int temp = m[i][k];
+                m[i][k] = m[i][j];
+                m[i][j]= temp;
+                j++;
+                k--;
+            }
+        }
+
+    }
+}
