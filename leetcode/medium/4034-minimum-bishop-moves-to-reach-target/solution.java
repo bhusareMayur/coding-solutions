0ecde65b5a7454223cf735e7a381f9ce074722c0
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-21 14:27:15 +0530

    ✅ Minimum Bishop Moves to Reach Target | Medium | Java | Runtime: 1 ms (94.92%) | Memory: 43.8 MB | 2026-09-21

--- leetcode/medium/4034-minimum-bishop-moves-to-reach-target/solution.java
+++ leetcode/medium/4034-minimum-bishop-moves-to-reach-target/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minBishopMoves(int[] s, int[] t) {
-        if(( s[0] + s[1]+ t[0]+t[1] ) % 2 != 0) return -1;
+        if(( s[0] + s[1]+ t[0]+t[1] ) % 2 != 0) return -1;// unreachable
 
-        if(Math.abs(s[0] - t[0]) == Math.abs(s[1] - t[1])) return 1;
+        if(Math.abs(s[0] - t[0]) == Math.abs(s[1] - t[1])) return 1;//on same diagonal
 
-        return 2;
+        return 2;// if not -1, 1 then default 2
     }
 }
