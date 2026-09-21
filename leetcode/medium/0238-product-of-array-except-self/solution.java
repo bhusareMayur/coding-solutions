bb0407eaf6b028a869198a8ccbfbb9edc795396c
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-21 11:54:46 +0530

    ✅ Product of Array Except Self | Medium | Java | Runtime: 353 ms (2.83%) | Memory: 70.3 MB | 2026-09-21

--- leetcode/medium/0238-product-of-array-except-self/solution.java
+++ leetcode/medium/0238-product-of-array-except-self/solution.java
@@ -3,25 +3,25 @@ class Solution {
         int n = nums.length;
 
         int[] p = new int[n];
-        // int s = 1;
-        int[] s = new int[n];
         p[0]=1;
-        s[n-1] = 1;
+        int s = 1;
+        // int[] s = new int[n];
+        // s[n-1] = 1;
         for(int i = 1 ; i <n;i++){
             p[i] = p[i-1]*nums[i-1];
             System.out.print(p[i]+" , ");
         }
-        for(int i =n-2 ; i >=0;i--){
-            s[i] = s[i+1]*nums[i+1];
-            // System.out.print(s[i]+" , ");
-        }
-        for(int i = 0 ; i < n;i++){
-            p[i] = p[i] * s[i];
-        }
-        // for(int i = n-1 ; i >=0;i--){
-        //     p[i] *= s;
-        //     s *= nums[i];
+        // for(int i =n-2 ; i >=0;i--){
+        //     s[i] = s[i+1]*nums[i+1];
+        //     // System.out.print(s[i]+" , ");
         // }
+        // for(int i = 0 ; i < n;i++){
+        //     p[i] = p[i] * s[i];
+        // }
+        for(int i = n-1 ; i >=0;i--){
+            p[i] *= s;
+            s *= nums[i];
+        }
 
         return p;
         
