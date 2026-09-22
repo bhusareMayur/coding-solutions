e42853721deb4d3bafdd3420a7d21276b3f3554d
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-22 20:42:48 +0530

    ✅ Linked List Cycle II | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 46.9 MB | 2026-09-22

--- leetcode/medium/0142-linked-list-cycle-ii/solution.java
+++ leetcode/medium/0142-linked-list-cycle-ii/solution.java
@@ -16,7 +16,7 @@ public class Solution {
       ListNode fast = head;
       boolean flag = false;
 
-      while(fast.next != null && fast != null){
+      while(fast != null && fast.next != null){
         slow = slow.next;
         fast = fast.next.next;
         if(slow == fast){
