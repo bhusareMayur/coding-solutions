04853c6553fb2e2ef6999b755c12ac295a2656d0
Mayur Bhusare <mayurbhusare8262@gmail.com>
2026-09-23 10:43:55 +0530

    ✅ Search in Linked List | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- gfg/basic/search-in-linked-list-1664434326/solution.java
+++ gfg/basic/search-in-linked-list-1664434326/solution.java
@@ -12,11 +12,15 @@ class Node {
 class Solution {
     public boolean searchKey(Node head, int key) {
         // Code here
-        Node temp = head;
-        while(temp != null){
-            if(temp.data == key) return true;
-            temp = temp.next;
-        }
-        return false;
+        // Node temp = head;
+        // while(temp != null){
+        //     if(temp.data == key) return true;
+        //     temp = temp.next;
+        // }
+        // return false;
+        if(head == null) return false;
+        if(head.data == key) return true;
+        
+        return searchKey(head.next , key);
     }
 }
